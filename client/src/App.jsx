eeb10cb8f65b70a3617f51bc3bcea7a239cf9ed1
the-code-bottle <ayushverma.iitkgp@gmail.co>
2024-01-25 14:22:26 +0530

    pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react'; 
+import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
+import Projects from './pages/projects';
+import Home from './pages/home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/dashboard';
+import About from './pages/About';
+
+
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/' element={<Home />} />
+        <Route path='sign-in' element={<SignIn />} />
+        <Route path='sign-up' element={<SignUp />} />
+        <Route path='dashboard' element={<Dashboard />}/>
+        <Route path='about' element={<About />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
